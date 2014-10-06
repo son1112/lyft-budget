b0138a6cea8817d18140193b10ec2049a175ea44
son <son@lincolnix.net>
2014-10-06 22:17:04 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LYFTbudget::Application.config.secret_key_base = '03beb215d52cb8aa5dea36a3ca4825e8bf4e912f2698e99f913e7c448d77e602e3ce938064daec44c7d2509f3c791d59bb7bae8a9180a38dde20d4a8defbcfa2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LYFTbudget::Application.config.session_store :cookie_store, key: '_LYFTbudget_session'
